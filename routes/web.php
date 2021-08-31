bb94b71f421aaefa10bb38636a39a6dfd4adf747
Ziv <renfuliau@gmail.com>
2021-08-31 18:09:11 +0800

    route update

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@ use \Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'FrontendController@index');
 
 Route::get('/test', function () {
     // App::setLocale('en');
